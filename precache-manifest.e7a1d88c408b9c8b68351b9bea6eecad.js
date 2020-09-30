52b8ee66770663f0b80a4ec7a9d161e54ffcea77
achal <achal.kumar@oyorooms.com>
2020-09-30 13:37:02 +0530

    Updates

---
+++ precache-manifest.e7a1d88c408b9c8b68351b9bea6eecad.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f091b0fea323ac7b18e0a5d4677cdb16",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "91489c53cfab5fd40154",
+    "url": "/weatherapp/static/css/main.602a46fc.chunk.css"
+  },
+  {
+    "revision": "483762db0974c41ade57",
+    "url": "/weatherapp/static/js/2.50d70231.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weatherapp/static/js/2.50d70231.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91489c53cfab5fd40154",
+    "url": "/weatherapp/static/js/main.f0136946.chunk.js"
+  },
+  {
+    "revision": "5cc9db4686e2547a8d0d",
+    "url": "/weatherapp/static/js/runtime-main.8a6508ad.js"
+  },
+  {
+    "revision": "8c62380ef1b1be1f811059efcffe329a",
+    "url": "/weatherapp/static/media/clouds.8c62380e.jpg"
+  },
+  {
+    "revision": "93cb2dee8c8ffcd1a0d93948b43d032f",
+    "url": "/weatherapp/static/media/haze.93cb2dee.jpg"
+  },
+  {
+    "revision": "3a57c724c475ab69fbced9ddf70d3132",
+    "url": "/weatherapp/static/media/sunny.3a57c724.jpg"
+  },
+  {
+    "revision": "d228f7519af97c382a4876b4a6d700c0",
+    "url": "/weatherapp/static/media/weatherdef.d228f751.jpg"
+  },
+  {
+    "revision": "49bf225c7b1697f40fc7a8e6ec3ed84b",
+    "url": "/weatherapp/static/media/winter.49bf225c.jpeg"
+  }
+]);
